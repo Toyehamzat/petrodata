2de8f7067cba6916bc4a9241f3f8b604c194f1d5
Toyehamzat <anibiseun@gmail.com>
2025-01-21 19:52:51 +0100

    feat: update header component to display dynamic page titles based on pathname

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -11,10 +11,20 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "../toggle/theme-toggle";
 import { useSidebar } from "../ui/sidebar";
+import { usePathname } from "next/navigation";
 
 const Header: FC = () => {
   const { isMobile, toggleSidebar } = useSidebar();
-
+  const pathname = usePathname();
+  const pathToTitleMap: Record<string, string> = {
+    "/dashboard": "Dashboard",
+    "/analysis": "Analysis",
+    "/settings": "Settings",
+    "/watchlist": "Watchlist",
+    "/news": "News and Report",
+  };
+  
+  const title = pathToTitleMap[pathname] || "Page Not Found";
   return (
     <header className="flex items-center justify-between w-full h-[60px] px-0">
       {/* Left Section */}
@@ -31,7 +41,7 @@ const Header: FC = () => {
         )}
         <div>
           <h1 className="text-lg md:text-2xl font-medium dark:text-[#F5F5F5] text-primary">
-            Analysis
+            {title}
           </h1>
           <p className="text-xs md:text-base font-normal -mt-1 md:mt-0 text-[#A3A3A3]">
             Thursday, February 15
