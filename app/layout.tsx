29ef940f0230b18bddc5ff9524166fd34de19813
Toyehamzat <anibiseun@gmail.com>
2025-01-21 19:31:03 +0100

    feat: add Header component to RootLayout for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/provider/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/AppSidebar";
 import { cookies } from "next/headers";
+import Header from "@/components/header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
   description: "We store and manage Petroleum and financial data",
 };
 
-export default async  function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -31,13 +32,16 @@ export default async  function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[calc(100vh-1px)] overflow-y-scroll`}
       >
         <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
-          <SidebarProvider defaultOpen={defaultOpen} >
+          <SidebarProvider defaultOpen={defaultOpen}>
             <AppSidebar />
-            <main>
-              {children}
+            <main className="px-[10px] md:px-[72px] py-6 md:py-12 w-full max-w-[1300px] m-auto flex min-h-screen h-full">
+              <div className="flex flex-col gap-2 w-full h-full">
+                <Header />
+                {children}
+              </div>
             </main>
           </SidebarProvider>
         </ThemeProvider>
