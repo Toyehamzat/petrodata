68c07d2b62e86e519a3d22291ee3203b1e07b89c
Toyehamzat <anibiseun@gmail.com>
2025-01-21 23:09:29 +0100

    feat: wrap layout in MantineProvider for enhanced theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/AppSidebar";
 import { cookies } from "next/headers";
 import Header from "@/components/header/header";
+import { MantineProvider } from "@mantine/core";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,17 +35,23 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[calc(100vh-1px)] overflow-y-scroll`}
       >
-        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
-          <SidebarProvider defaultOpen={defaultOpen}>
-            <AppSidebar />
-            <main className="px-[10px] md:px-[72px] py-6 md:py-12 w-full max-w-[1440px] m-auto flex min-h-screen h-full">
-              <div className="flex flex-col gap-2 w-full h-full">
-                <Header />
-                {children}
-              </div>
-            </main>
-          </SidebarProvider>
-        </ThemeProvider>
+        <MantineProvider>
+          <ThemeProvider
+            attribute="class"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SidebarProvider defaultOpen={defaultOpen}>
+              <AppSidebar />
+              <main className="px-[10px] md:px-[72px] py-6 md:py-12 w-full max-w-[1440px] m-auto flex min-h-screen h-full">
+                <div className="flex flex-col gap-10 w-full h-full">
+                  <Header />
+                  {children}
+                </div>
+              </main>
+            </SidebarProvider>
+          </ThemeProvider>
+        </MantineProvider>
       </body>
     </html>
   );
