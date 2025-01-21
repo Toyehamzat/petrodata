87019967b353fd01771475b98c52f207e17a6fd1
Toyehamzat <anibiseun@gmail.com>
2025-01-21 23:14:33 +0100

    feat: update TabsTrigger styles for improved accessibility and visual feedback

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center outline-none whitespace-nowrap px-3 py-2.5 text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50  data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap px-3 py-2.5 text-sm font-medium focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground",
       className
     )}
     {...props}
