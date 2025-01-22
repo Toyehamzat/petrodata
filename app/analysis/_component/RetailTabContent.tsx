12b4b18a5c90b4471ca85c1f0d0b5291e65da8d0
Toyehamzat <anibiseun@gmail.com>
2025-01-22 11:46:48 +0100

    feat: enhance RetailTabContent with search functionality and WeeklyReport component

--- app/analysis/_component/RetailTabContent.tsx
+++ app/analysis/_component/RetailTabContent.tsx
@@ -1,17 +1,33 @@
-"use client"
+"use client";
 import React from "react";
 import ProductPrices from "./ProductPrices";
-import { useSidebar } from "@/components/ui/sidebar";
-
+import WeeklyReport from "./WeeklyReport";
+import { LucideSearch } from "lucide-react";
+import { Input } from "@/components/ui/input";
 export default function RetailTabContent() {
-  const {open} = useSidebar();
   return (
-    <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
-      <div className={`${open ? "w-[500px]" : "w-[580px]"}`}>
-        <ProductPrices/>
+    <div className="w-full flex flex-col gap-8">
+      <div className={`grid gap-4 grid-cols-1 md:grid-cols-[2fr_3fr]`}>
+        <div className="w-full">
+          <ProductPrices />
+        </div>
+        <div className="w-full">
+          <ProductPrices />
+        </div>
       </div>
-      <div className={`${open ? "w-[585px]" : "w-[600px]"}`}>
-        <ProductPrices/>
+
+      <div className="w-full flex flex-col gap-8 border-t-[1px] py-6 border-[#464646]">
+        <div className="relative w-80 h-11">
+          <LucideSearch className="absolute left-3 top-[22px] transform -translate-y-1/2 h-5 w-5 text-[#A3A3A3]" />
+          <Input
+            type="text"
+            placeholder="Search..."
+            className="bg-[#525252] border-none text-[#A3A3A3] pl-9 pr-4 rounded-2xl h-full w-full"
+          />
+        </div>
+        <div>
+          <WeeklyReport />
+        </div>
       </div>
     </div>
   );
